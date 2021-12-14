dc6507bd89b8b4d8c1f4e8229a0555b5ff4d19e1
BlueGrisGris <emanninen@college.harvard.edu>
2021-12-14 13:34:17 -0500

    unit test lib.earthDistances

---
+++ tests/test_lib.py
@@ -0,0 +1,36 @@
+"""
+Test lib functions  
+"""
+### import requirements 
+import sys
+import pytest
+import numpy as np
+
+### set path above Barcast
+sys.path.append("./src/")
+### import module
+import Barcast
+### testing class
+class TestLib():
+
+    def test_earthDistances(self):
+        """
+        unit test earthDistances()
+        """
+        ### pick 2 points to calc distance: cambridge ma to tucson az
+        locations = np.array([[32,-110], [42,-71]])
+        official_dist = np.array([[0,3606],[3606,0]])  ## rounding to the nearest km?
+        calc_dist = np.round(Barcast.earthDistances(locations),0)
+
+        print(f"calc_dist: {calc_dist}")
+        print(f"official_dist: {official_dist}")
+
+        assert np.array_equal(calc_dist , official_dist) == True
+
+
+        
+
+
+
+
+
